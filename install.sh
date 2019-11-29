d8dc7a0fce4f2bb918e865202d43365abca76e61
Jay <11070389+PlatinumJay@users.noreply.github.com>
2019-11-29 09:43:41 -0500

    Update install.sh
    Condensed sed into function loop. Updated sed identifier.
    Will rename file and create zip.

--- install.sh
+++ install.sh
@@ -14,8 +14,29 @@ echo -e "#                 the plugin Template.                     #"
 echo -e "#==========================================================#"
 tput setaf 6
 
-a0=("Enter the plugin name..." "Enter the plugin URI..." "Enter description for the plugin..." "Enter author for the plugin..." "Enter author URI for the plugin..." "Enter dashicon for the plugin..." "Enter security role for plugin...")
-a1=(wp_name wp_uri wp_desc wp_author wp_author_uri wp_dashicon wp_access)
+a0=("Enter the plugin name..."
+	"Enter the plugin URI..."
+	"Enter description for the plugin..." 
+	"Enter author for the plugin..." 
+	"Enter author URI for the plugin..." 
+	"Enter dashicon for the plugin (https://developer.wordpress.org/resource/dashicons/)..." 
+	"Enter security role for plugin...")
+
+a1=(wp_name 
+	wp_uri 
+	wp_desc 
+	wp_author 
+	wp_author_uri 
+	wp_dashicon 
+	wp_access)
+
+a2=("PLUGINNAME"
+	"PLUGINURI"
+	"DESCNAME"
+	"AUTHORNAME"
+	"AUTHORURI"
+	"PLUGINDASHICON"
+	"ACCESSNAME")
 
 setup_vars () {
    tput setaf 6
@@ -23,6 +44,10 @@ setup_vars () {
    
    tput setaf 7
    read $2
+}
+
+setup_replc () {
+   sed -i "s!$1!${!2}!g" $file
 }
 
 for ((i=0;i<"${#a0[@]}";i++))
@@ -34,15 +59,17 @@ wp_name_lower="${wp_name,,}"
 wp_name_under=${wp_name_lower// /_}
 wp_name_dash=${wp_name_lower// /-}
 
-sed -i "s/PLUGINDASHICON/$wp_dashicon/g" $file
-sed -i "s/PLUGINNAME/$wp_name/g" $file
-sed -i "s/PLUGINURI/$wp_uri/g" $file
-sed -i "s/DESCNAME/$wp_desc/g" $file
-sed -i "s/PLUGIND/$wp_name_dash/g" $file
-sed -i "s/PLUGIN_NAME/$wp_name_under/g" $file
-sed -i "s/AUTHORNAME/$wp_author/g" $file
-sed -i "s/AUTHORURI/$wp_author_uri/g" $file
-sed -i "s/ACCESSNAME/$wp_access/g" $file
+for ((i=0;i<"${#a0[@]}";i++))
+do
+   setup_replc "${a2[$i]}" ${a1[$i]}
+done
+
+
+sed -i "s!PLUGIND!$wp_name_dash!g" $file
+sed -i "s!PLUGIN_NAME!$wp_name_under!g" $file
+
+mv wordpress-plugin-template.php ${wp_name_dash}.php
+zip ${wp_name_dash}.php.zip ${wp_name_dash}.php
 
 tput setaf 5
 tput bold 
